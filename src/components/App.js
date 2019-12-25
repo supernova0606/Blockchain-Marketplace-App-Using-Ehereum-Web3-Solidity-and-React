8d6aef1dfad39009ec9e838653012e3f9c8e83c0
supernova0606 <drivendev66@gmail.com>
2019-12-26 00:57:49 +0500

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -13,7 +13,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Dapp University
+            Marketplace
           </a>
         </nav>
         <div className="container-fluid mt-5">
